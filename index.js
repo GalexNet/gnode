ba708b4b0f0e3a6eee331411e31513ac96cc5119
Alex Gale <alex.gale@ocasta.com>
2023-07-13 20:27:05 +0100

    Add config loader and local docker dev configs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('GNODE')
+const express = require('express')
+const { loadConfig } = require('./utils/config/config-utils')
+
+const app = express()
+const port = 8080
+
+app.locals.config = loadConfig()
+console.log(app.locals)
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
